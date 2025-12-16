8d834e063bcaf58968e78ae4184f8d64fc8639f4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-12-16 13:54:57 -0500

    create class avg calc

--- grades.c
+++ grades.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 float calcAvg(int grades[], int size) {
-    int gradeSum;
+    int gradeSum=0;
     for (int i = 0; i < size; i++) {
         gradeSum += grades[i];
     }
@@ -14,5 +14,10 @@ int main() {
         {80, 85, 90},
         {70, 75, 80}
     };
+
+    for (int i = 0; i < 3; i++) {
+        float classAvg = calcAvg(grades[i], 3);
+        printf("The Student's avg is: %f.\n", classAvg);
+    }
     return 0;
 }
