b86739c3cca386287997a244a50f94b7f30fa5c2
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-12-17 09:58:25 -0500

    create guessNum

--- guess.c
+++ guess.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void guessNum(int num) {
+    if (num == 555) {
+        printf("Correct!\n");
+    } else {
+        printf("WRONG\n");
+    }
+}
+
 int main() {
 
     return 0;
