0278b566be1bf829bcb6aa380848735440ee07c3
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2026-01-08 13:53:02 -0500

    reset

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char first[20] = "Jimothy";
-    char last[5] = "Smith";
-    printf("Hello, %s", strncat(first,last,5));
-    return 0;
-}
