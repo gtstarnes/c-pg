057d6ea34bf2142c860fc02e22c3d7f6669d7461
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2026-01-20 16:21:06 -0500

    create head node

--- linkedList.c
+++ linkedList.c
@@ -6,11 +6,13 @@ struct Node {
     struct Node* next; 
 };
 
-void insert(struct Node** head_ref, int new_data) {
+struct Node* head = NULL;
+
+void insert(int new_data) {
     struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
     new_node->data = new_data;
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
+    new_node->next = head;
+    head = new_node;
 }
 
 int main() {
