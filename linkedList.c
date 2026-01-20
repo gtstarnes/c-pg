d62fdaa49e5e4f07b94801fcced5614fec1af549
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2026-01-20 16:19:24 -0500

    create insert function

--- linkedList.c
+++ linkedList.c
@@ -6,6 +6,12 @@ struct Node {
     struct Node* next; 
 };
 
+void insert(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
 
 int main() {
     return 0;
