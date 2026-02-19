515e63634eb0783655697c1c18dd011e543c8e21
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2026-02-19 12:29:26 -0500

    create get min

--- algos.c
+++ algos.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+int getMin(int a[]){
+    int min = a[0];
+    int len = sizeof(a) / sizeof(a[0]);
+    for (int i = 0; i < len; i++) {
+        if (a[i] < min) {
+            min = a[i];
+        }
+    }
 
+    return min;
+}
 
 int main() {
     int a[50];
